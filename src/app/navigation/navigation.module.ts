f4b0ec2e0fc90a3a9a8bedc07b8e4a1aa551cfbb
Reginaldo Campelo <regin.developer@gmail.com>
2022-05-07 19:42:49 -0300

    adding footer-component

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
