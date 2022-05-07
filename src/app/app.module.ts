309b2d95140bb54b2e7244aa706217c960580cc5
Reginaldo Campelo <regin.developer@gmail.com>
2022-05-07 19:41:09 -0300

    adding navigation module to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NavigationModule } from './navigation/navigation.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavigationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
