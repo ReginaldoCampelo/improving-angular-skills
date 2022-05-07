0e3005e5ba13f5e2891b7e170c1a416572b71b36
Reginaldo Campelo <regin.developer@gmail.com>
2022-05-07 19:43:47 -0300

    adding home-component

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
