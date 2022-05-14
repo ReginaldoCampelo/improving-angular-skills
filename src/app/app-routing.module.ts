53715294ec8b7f32083deefc1db8290d43781b89
Reginaldo Campelo <regin.developer@gmail.com>
2022-05-14 14:55:44 -0300

    adding settings for account routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AccountModule } from './account/account.module';
 import { HomeComponent } from './navigation/home/home.component';
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => AccountModule) },
+
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent }
   
 ];
 
