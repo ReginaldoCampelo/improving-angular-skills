54796d5ea4484595b234c28e585d9eb4776c8862
Reginaldo Campelo <regin.developer@gmail.com>
2022-05-14 14:55:55 -0300

    adding account module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AccountRouteModule } from './account.route';
+
+import { AccountAppComponent } from './account.app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
 
-
 @NgModule({
   declarations: [
+    AccountAppComponent,
     RegisterComponent,
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    AccountRouteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AccountModule { }
