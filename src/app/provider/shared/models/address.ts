3fefab995ea11e9c5a2724dce22eb4613c3338be
Reginaldo Campelo <regin.developer@gmail.com>
2022-06-07 19:10:49 -0300

    adding address class

---
+++ src/app/provider/shared/models/address.ts
@@ -0,0 +1,11 @@
+export class Address {
+    id: string;
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cep: string;
+    cidade: string;
+    estado: string;
+    fornecedorId: string;
+}
