2633fc87ce1adf3ea659998115fb648f8a196f14
Reginaldo Campelo <regin.developer@gmail.com>
2022-06-07 19:12:28 -0300

    adding provider app component

--- src/app/provider/provider.module.ts
+++ src/app/provider/provider.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ProviderRoutingModule } from './provider-routing.module';
+import { ProviderService } from './shared/service/provider.service';
 
 import { NewComponent } from './components/new/new.component';
 import { ListComponent } from './components/list/list.component';
 import { EditComponent } from './components/edit/edit.component';
 import { DeleteComponent } from './components/delete/delete.component';
 import { DetailComponent } from './components/detail/detail.component';
+import { ProviderAppComponent } from './provider.app.component';
+
+
 
 
 @NgModule({
   declarations: [
+    ProviderAppComponent,
     NewComponent,
     ListComponent,
     EditComponent,
@@ -20,7 +27,10 @@ import { DetailComponent } from './components/detail/detail.component';
     ],
   imports: [
     CommonModule,
-    ProviderRoutingModule
+    ProviderRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [
     NewComponent,
@@ -28,6 +38,9 @@ import { DetailComponent } from './components/detail/detail.component';
     EditComponent,
     DeleteComponent,
     DetailComponent,
+  ],
+  providers: [
+    ProviderService
   ]
 })
 export class ProviderModule { }
