796e80e4e3f88c3ac0219cd9352f1372b96e96db
Reginaldo Campelo <regin.developer@gmail.com>
2022-06-07 19:13:05 -0300

    adding routing for provider component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => AccountModule) },
+  { path: 'providers', loadChildren: () => import('./provider/provider.module').then(m => m.ProviderModule) },
 
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', component: NotFoundComponent }
--- src/app/provider/provider-routing.module.ts
+++ src/app/provider/provider-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeleteComponent } from './components/delete/delete.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+import { NewComponent } from './components/new/new.component';
+import { ProviderAppComponent } from './provider.app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ProviderAppComponent,
+    children: [
+      { path: 'find/all', component: ListComponent },
+      { path: 'insert', component: NewComponent },
+      { path: 'update/:id', component: EditComponent },
+      { path: 'detail/:id', component: DetailComponent },
+      { path: 'delete/:id', component: DeleteComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
