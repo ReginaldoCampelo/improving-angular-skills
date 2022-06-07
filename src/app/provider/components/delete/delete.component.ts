e3d5e0c9229ca2a5e751799b8954e6be550c2f35
Reginaldo Campelo <regin.developer@gmail.com>
2022-06-07 20:07:18 -0300

    improving delete provider component

--- src/app/provider/components/delete/delete.component.ts
+++ src/app/provider/components/delete/delete.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Provider } from '../../shared/models/provider';
+import { ProviderService } from '../../shared/service/provider.service';
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
   styleUrls: ['./delete.component.css']
 })
-export class DeleteComponent implements OnInit {
+export class DeleteComponent {
 
-  constructor() { }
+  provider: Provider = new Provider();
 
-  ngOnInit(): void {
+  constructor(private providerService: ProviderService, private route: ActivatedRoute, private router: Router, private toastr: ToastrService) {
+    
+    this.providerService.getById(route.params['id'])
+    .subscribe(provider => this.provider = provider);
   }
 
+  deleteEvent() {
+    this.providerService.deleteProvider(this.provider.id)
+    .subscribe(
+      event => { this.actionForSuccess(event) },
+      error => { this.actionForFail() }
+    );
+  }
+
+  actionForSuccess(event: any) {
+    const toast = this.toastr.success('Fornecedor excluído com sucesso!', 'Good bye :D');
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.router.navigate(['/providers/find/all']);
+      })
+    }
+  }
+
+  actionForFail() {
+    this.toastr.error('Houve um erro no processamento!', 'Ops! :(');
+  }
 }
