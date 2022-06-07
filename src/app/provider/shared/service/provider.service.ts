2957dcba73affe90204d6314a3fee8a226db287b
Reginaldo Campelo <regin.developer@gmail.com>
2022-06-07 19:11:15 -0300

    adding provider service

--- src/app/provider/shared/service/provider.service.ts
+++ src/app/provider/shared/service/provider.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+
+import { BaseService } from 'src/app/shared/services/base.service';
+import { Provider } from '../models/provider';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProviderService {
+export class ProviderService extends BaseService {
 
-  constructor() { }
+  provider: Provider = new Provider();
+
+  constructor(private http: HttpClient) {
+    super();
+
+    this.provider.nome = "Teste fake"
+    this.provider.documento = "123456789"
+    this.provider.ativo = true
+    this.provider.tipoFornecedor = 1
+  }
+
+  getAll(): Observable<Provider[]> {
+    return this.http.get<Provider[]>(this.UrlServiceV1 + "fornecedores")
+      .pipe(catchError(super.serviceError));
+  }
+
+  getById(id: string): Observable<Provider> {
+    return new Observable<Provider>();
+  }
+
+  insertProvider(provider: Provider): Observable<Provider> {
+    return new Observable<Provider>();
+  }
+
+  updateProvider(provider: Provider): Observable<Provider> {
+    return new Observable<Provider>();
+  }
+
+  deleteProvider(id: string): Observable<Provider> {
+    return new Observable<Provider>();
+  }
 }
